be4203af7438ec3dc86519432137fc3f4a25b37f
craigongit <238895450+craigongit@users.noreply.github.com>
2026-04-24 17:43:30 +0300

    Implement option selector

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Spectre.Console;
+
+string[] menuChoices = 
+[
+    "View Books", 
+    "Add Book", 
+    "Delete Book"
+];
+
+var choice = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+    .Title("Select your next Operation")
+    .AddChoices(menuChoices));
