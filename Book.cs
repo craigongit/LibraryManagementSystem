5a09cce1f31416b1a55e424ce9dd0ba1c95ab4fa
Craig B. Ndagano <238895450+craigongit@users.noreply.github.com>
2026-05-04 20:00:52 +0300

    Create Book model class

---
+++ Book.cs
@@ -0,0 +1,14 @@
+namespace LibraryManagementSystem;
+
+internal class Book
+{
+    public string Name {get; set;} = "Unknown";
+    public int Pages {get; set;} = 0;
+
+    internal Book(string name, int pages)
+    {
+        Name = name;
+        Pages = pages;
+        Console.WriteLine($"Name: {Name}, Pages: {Pages}");
+    }
+}
